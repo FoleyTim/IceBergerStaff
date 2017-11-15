d5128e035be204bc9cb6502022434b388d677c4e
FoleyTim <teefoles@gmail.com>
2017-11-15 14:15:54 +1300

    deleted burger gram pics. added staff account functionality(add and remove accounts). made customer table 2 rows as type column no longer needed. added log in failure alert. added stock update success alert.

--- script.js
+++ script.js
@@ -13,71 +13,111 @@ function openMenu(evt, item) {
 }
 
 
-
+//LOG IN
 $('document').ready(function() {
   $("#loginform").submit(function(event) {
     event.preventDefault();
-
     var name = document.getElementById("logUname").value;
     var password = document.getElementById("logPsw").value;
     $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=login&email=" + name + "&password=" + password, function(data) {
       if (data['success'] && data['privilege'] > 0) {
         $.cookie("user", data['user']);
         window.location.href = "index.html";
+      }else{
+        alert("incorrect username or password");
       }
     });
-
     return false;
   });
 
+
+  //SIGN UP
+  $("#createStaffButton").click(function(event) {
+    event.preventDefault();
+    var name = document.getElementById("signUname").value;
+    var password = document.getElementById("signPsw").value;
+    var email = document.getElementById("signEmail").value;
+    $.getJSON("https://iceberger.ey.nz/api.php?callback=?", "method=registerstaff&name=" + name + "&password=" + password + "&email=" + email, function(data) {
+      console.log(data);
+      if (data['success']) {
+        alert("new account created");
+      }
+    });
+    return false;
+  });
+  //LOG OUT BUTTON
   $("#logout").click(function(event) {
     event.preventDefault();
-
     $.removeCookie("user");
     window.location.href = "Login.html";
-
     return false;
   });
 
+//POPULATE STOCKLIST
   $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=getinventory", function(data) {
-    // console.log(data);
     $('#inventory').empty();
     data['inventory'].forEach(function(category) {
-      // console.log(category);
       $('#inventory').append("<tr><th>"+category['description']+"</th><th></th></tr>");
       category['items'].forEach(function(item) {
         $('#inventory').append("<tr><td>"+item['name']+"</td><td><input class=\"inventoryitem\" type=\"text\" name=\""+item['id']+"\" value=\""+item['stock_count']+"\"></td></tr>");
       });
-      // console.log(item);
-      // $('#inventory').append("<tr><td>"+item['name']+"</td><td><input class=\"inventoryitem\" type=\"text\" name=\""+item['id']+"\" value=\""+item['stock_count']+"\"></td></tr>");
     });
   });
 
-  $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=getusers", function(data) {
-    // console.log(data);
+//POPULATE CUSTOMER LIST
+  $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=getcustomers", function(data) {
     $('#customerList').empty();
     data.forEach(function(user) {
-      $('#customerList').append("<tr><td>"+user['name']+"</td><td>"+user['email']+"</td><td>"+(user['privilege'] == 0 ? "Customer" : "Staff")+"</td></tr>");
+      $('#customerList').append("<tr><td>"+user['name']+"</td><td>"+user['email']+"</td></tr>");
+    });
+  });
+
+  //POPULATE STAFF LIST
+  $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=getstaff", function(data) {
+    $('#staffList').empty();
+    data.forEach(function(user) {
+      var row = document.createElement('tr');
+      var col1 = document.createElement('td');
+      var col2 = document.createElement('td');
+      row.appendChild(col1);
+      row.appendChild(col2);
+      col1.appendChild(document.createTextNode(user['name']));
+
+      var btn = document.createElement('button');
+      btn.appendChild(document.createTextNode('remove'));
+      col2.appendChild(btn);
+
+      btn.addEventListener('click', function(e) {
+        console.log('remove press');
+        $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=deleteuser&user=" + user['id'], function(data) {
+              document.getElementById('staffList').removeChild(row);
+        });
+      });
+
+      document.getElementById('staffList').appendChild(row);
+      // $('#staffList').append("<tr><td>"+user['name']+"</td><td><button>remove</button></td></tr>");
     });
   });
 
 
+  function showMsgOnUpdate(){
+   alert("the database has been updated!");
+  }
 
+//UPDATE BUTTON CLICK EVENT
   $("#updateButton").click(function(event) {
     event.preventDefault;
-
     var json = {};
-
     $(".inventoryitem").each(function() {
       var id = $(this).attr("name");
       var count = parseInt($(this).val());
       json[id] = count;
     })
-
     console.log(JSON.stringify(json));
 
     $.getJSON("http://iceberger.ey.nz/api.php?callback=?", "method=updateinventory&stock=" + JSON.stringify(json), function(data) {
       console.log(data);
+      showMsgOnUpdate();
     });
   });
 });
